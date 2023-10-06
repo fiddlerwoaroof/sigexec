229238052c623f0f7f664d9c3efff9b1f5ced57f
Edward Langley <el-github@elangley.org>
2023-10-06 00:46:18 -0700

    chore: more flake improvements

--- flake.nix
+++ flake.nix
@@ -12,39 +12,47 @@
     self,
     nixpkgs,
     flake-utils,
-  }:
+  }: let
+  in
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
       };
+      build_zig = deriv @ {nativeBuildInputs ? [], ...}:
+        pkgs.stdenv.mkDerivation ({
+            dontConfigure = true;
+
+            preBuild = ''
+              export HOME=$TMPDIR
+            '';
+
+            installPhase = ''
+              runHook preInstall
+              zig build -Drelease-safe -Dcpu=baseline --prefix $out install
+              runHook postInstall
+            '';
+          }
+          // deriv
+          // {
+            nativeBuildInputs = [pkgs.zig_0_10] ++ nativeBuildInputs;
+          });
       socat = pkgs.socat;
-    in rec {
+    in {
       devShells.default = pkgs.mkShell {
         buildInputs = [pkgs.zig_0_10 socat];
       };
-      devShell = devShells.default;
-      packages.default = pkgs.stdenv.mkDerivation {
+      devShell = self.devShells.default;
+      packages.default = build_zig {
         pname = "sigexec";
         version = "0.0.2";
         src = ./.;
 
-        dontConfigure = true;
-
-        preBuild = ''
-          export HOME=$TMPDIR
-        '';
-
-        installPhase = ''
-          runHook preInstall
-          zig build -Drelease-safe -Dcpu=baseline --prefix $out install
-          runHook postInstall
-        '';
-
-        nativeBuildInputs = [pkgs.zig_0_10];
-      };
-      apps.default = {
-        type = "app";
-        program = "${self.packages.${system}.default}/bin/sigexec";
+        meta = with pkgs.lib; {
+          homepage = "https://github.com/fiddlerwoaroof/sigexec";
+          description = "A simple utility that runs a command with each line sent over a socket.";
+          license = licenses.mit;
+          platforms = platforms.linux ++ platforms.darwin;
+        };
       };
     });
 }
